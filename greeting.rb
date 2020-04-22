5e4518c071b62d577cab3350fda12e3e5ec91c31
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-22 16:42:15 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,16 @@
+def greeting
+  puts "Hello World"
+end
+
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+
+cars = { toyota: ["avalon","camry"], honda: ["civic", "accord"] }
+cars[:toyota][]
